ab7692d72f1f83fb3b9d3a874c369447da6b7435
nlpxdc <nlpx_dc@hotmail.com>
2021-05-09 22:28:39 +0800

    draft

---
+++ src/main/java/io/cjf/testalgorithm/twosum/Solution.java
@@ -0,0 +1,14 @@
+package io.cjf.testalgorithm.twosum;
+
+public class Solution {
+    /**
+     *
+     * @param numbers int整型一维数组
+     * @param target int整型
+     * @return int整型一维数组
+     */
+    public int[] twoSum (int[] numbers, int target) {
+        // write code here
+        return null;
+    }
+}
