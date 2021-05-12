286f580f411eb0a0e0bfdd828bdd241fae1c9163
nlpxdc <nlpx_dc@hotmail.com>
2021-05-13 01:19:11 +0800

    fix

--- src/main/java/io/cjf/testalgorithm/fibonacci/Solution.java
+++ src/main/java/io/cjf/testalgorithm/fibonacci/Solution.java
@@ -5,11 +5,15 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
 //        int fibonacci4 = solution.Fibonacci(-1);
-        int fibonacci = solution.Fibonacci(0);
-        int fibonacci1 = solution.Fibonacci(1);
-        int fibonacci2 = solution.Fibonacci(2);
-        int fibonacci3 = solution.Fibonacci(3);
-        int fibonacci4 = solution.Fibonacci(4);
+//        int fibonacci = solution.Fibonacci(0);
+//        int fibonacci1 = solution.Fibonacci(1);
+//        int fibonacci2 = solution.Fibonacci(2);
+//        int fibonacci3 = solution.Fibonacci(3);
+//        int fibonacci4 = solution.Fibonacci(4);
+        for (int i = 0; i < 100; i++) {
+            int v = solution.Fibonacci(i);
+            System.out.println(String.format("n:%s, %s", i, v));
+        }
     }
 
     public int Fibonacci(int n) throws Exception {
