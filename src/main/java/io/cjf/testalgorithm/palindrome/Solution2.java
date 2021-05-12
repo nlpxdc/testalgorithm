89e6223bae8861bdf86fa84affdf0635f57fe8ea
nlpxdc <nlpx_dc@hotmail.com>
2021-05-13 03:01:41 +0800

    fix

---
+++ src/main/java/io/cjf/testalgorithm/palindrome/Solution2.java
@@ -0,0 +1,38 @@
+package io.cjf.testalgorithm.palindrome;
+
+import java.util.Stack;
+
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        boolean a = solution.judge("a");
+        boolean aa = solution.judge("aa");
+        boolean aba = solution.judge("aba");
+        boolean abba = solution.judge("abba");
+        boolean abcba = solution.judge("abcba");
+        boolean abc = solution.judge("abc");
+    }
+
+    public boolean judge (String str) {
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (chars.length % 2 == 0 || i != chars.length / 2) {
+                if (stack.empty()) {
+                    stack.push(c);
+                } else {
+                    Character t = stack.peek();
+                    if (c == t) {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
