a7bd57fdacab656dd8b2aa8203101416adda9d07
nlpxdc <nlpx_dc@hotmail.com>
2021-05-13 17:05:18 +0800

    fix

--- src/main/java/io/cjf/testalgorithm/MyApp.java
+++ src/main/java/io/cjf/testalgorithm/MyApp.java
@@ -3,6 +3,7 @@ package io.cjf.testalgorithm;
 public class MyApp {
     public static void main(String[] args) {
         String str = "aaaab";
+        //92567586
         System.out.println(str.hashCode());
     }
 }
