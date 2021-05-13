3c59ffd82e8f946929491be6be4eeb031bba886f
nlpxdc <nlpx_dc@hotmail.com>
2021-05-14 00:30:26 +0800

    fix

--- src/main/java/io/cjf/testalgorithm/MyApp.java
+++ src/main/java/io/cjf/testalgorithm/MyApp.java
@@ -5,5 +5,16 @@ public class MyApp {
         String str = "aaaab";
         //92567586
         System.out.println(str.hashCode());
+
+        int a = 1;
+        int b = 2;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        int i = Integer.bitCount(3);
+        int i1 = Integer.highestOneBit(3);
+        String s = Integer.toBinaryString(3);
     }
+
 }
