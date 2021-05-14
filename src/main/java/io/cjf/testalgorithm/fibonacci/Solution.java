0c33685e4e1ed4ebb0292893e44fea20b827175f
nlpxdc <nlpx_dc@hotmail.com>
2021-05-14 14:09:21 +0800

    add fixed fibonacci

--- src/main/java/io/cjf/testalgorithm/fibonacci/Solution.java
+++ src/main/java/io/cjf/testalgorithm/fibonacci/Solution.java
@@ -5,9 +5,49 @@ public class Solution {
     private int[] fs = new int[40];
 
     public Solution() throws Exception {
-        for (int i = 0; i < 40; i++) {
-            fs[i] = fibonacci(i);
-        }
+        fs[0] = 0;
+        fs[1] = 1;
+        fs[2] = 1;
+        fs[3] = 2;
+        fs[4] = 3;
+        fs[5] = 5;
+        fs[6] = 8;
+        fs[7] = 13;
+        fs[8] = 21;
+        fs[9] = 34;
+
+        fs[10] = 55;
+        fs[11] = 89;
+        fs[12] = 144;
+        fs[13] = 233;
+        fs[14] = 377;
+        fs[15] = 610;
+        fs[16] = 987;
+        fs[17] = 1597;
+        fs[18] = 2584;
+        fs[19] = 4181;
+
+        fs[20] = 6765;
+        fs[21] = 10946;
+        fs[22] = 17711;
+        fs[23] = 28657;
+        fs[24] = 46368;
+        fs[25] = 75025;
+        fs[26] = 121393;
+        fs[27] = 196418;
+        fs[28] = 317811;
+        fs[29] = 514229;
+
+        fs[30] = 832040;
+        fs[31] = 1346269;
+        fs[32] = 2178309;
+        fs[33] = 3524578;
+        fs[34] = 5702887;
+        fs[35] = 9227465;
+        fs[36] = 14930352;
+        fs[37] = 24157817;
+        fs[38] = 39088169;
+        fs[39] = 63245986;
     }
 
     public static void main(String[] args) throws Exception {
@@ -21,26 +61,6 @@ public class Solution {
         for (int i = 0; i < 100; i++) {
             int v = solution.Fibonacci(i);
             System.out.println(String.format("n:%s, %s", i, v));
-        }
-    }
-
-    public int fibonacci(int n) throws Exception {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else if (n > 1) {
-            int f1 = 1;
-            int f2 = 0;
-            //todo calc f1f2
-            for (int i = 0; i < n - 2; i++) {
-                int origF1 = f1;
-                f1 += f2;
-                f2 = origF1;
-            }
-            return f1 + f2;
-        } else {
-            throw new Exception("invalid params");
         }
     }
 
