1937eb2d94415714855cdb818e8dcdcd8e853ba9
nlpxdc <nlpx_dc@hotmail.com>
2021-05-15 23:09:14 +0800

    add link bubble sort

--- src/main/java/io/cjf/testalgorithm/sort/linkselectsort/Solution.java
+++ src/main/java/io/cjf/testalgorithm/sort/linkselectsort/Solution.java
@@ -4,19 +4,26 @@ import java.util.*;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(3);
+        ListNode listNode3 = new ListNode(1);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = null;
+
+        ListNode listNode = new Solution().sortInList(listNode1);
+    }
+
     public ListNode sortInList (ListNode head) {
         // write code here
         for (ListNode i = head; i != null; i = i.next) {
-            ListNode min = i;
             for (ListNode j = i.next; j != null; j = j.next) {
-                if (min.val > j.val) {
-                    min = j;
+                if (i.val > j.val) {
+                    int t = i.val;
+                    i.val = j.val;
+                    j.val = t;
                 }
-            }
-            if (i != min) {
-                int t = i.val;
-                i.val = min.val;
-                min.val = t;
             }
         }
         return head;
