6c3c9c298a63868a859826092bb67881db670883
nlpxdc <nlpx_dc@hotmail.com>
2021-05-16 11:46:45 +0800

    fix

--- src/main/java/io/cjf/testalgorithm/sort/bubblesort/Solution3.java
+++ src/main/java/io/cjf/testalgorithm/sort/bubblesort/Solution3.java
@@ -7,9 +7,9 @@ public class Solution3 {
         int[] numbers2 = new int[]{1, 2, 3};
         Solution3 solution = new Solution3();
 
-        solution.bubbleSort(numbers);
+        solution.bubbleSort2(numbers);
         System.out.println("aaa");
-        solution.bubbleSort(numbers2);
+        solution.bubbleSort2(numbers2);
     }
 
     public void bubbleSort(int[] numbers) {
@@ -17,6 +17,22 @@ public class Solution3 {
             System.out.println(String.format("n: %s", i));
             for (int j = 0; j < i; j++) {
                 if (numbers[j] > numbers[j+1]) {
+                    int t = numbers[j+1];
+                    numbers[j+1] = numbers[j];
+                    numbers[j] = t;
+                }
+            }
+        }
+    }
+
+    public void bubbleSort2(int[] numbers) {
+        boolean beSort = false;
+        for (int i = numbers.length - 1; i > 0 && !beSort; i--) {
+            beSort = true;
+            System.out.println(String.format("n: %s", i));
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    beSort = false;
                     int t = numbers[j+1];
                     numbers[j+1] = numbers[j];
                     numbers[j] = t;
