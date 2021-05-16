285ff29df4ab0e6d535373d6fc6907b9b7c39cfc
nlpxdc <nlpx_dc@hotmail.com>
2021-05-16 21:58:54 +0800

    fix

--- src/main/java/io/cjf/testalgorithm/subarraymax/Solution.java
+++ src/main/java/io/cjf/testalgorithm/subarraymax/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public static void main(String[] args) {
         int[] arr = {1, -2, 3, 5, -2, 6, -1};
 
-        int maxsum = new Solution().maxsumofSubarray(arr);
+        int maxsum = new Solution().maxsumofSubarray2(arr);
     }
 
     /**
@@ -13,18 +13,37 @@ public class Solution {
      * @param arr int整型一维数组 the array
      * @return int整型
      */
-    public int maxsumofSubarray (int[] arr) {
-        int maxsum = Integer.MIN_VALUE;
+    //with index
+    public int maxsumofSubarray3 (int[] arr) {
+        int maxsum = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 int t = 0;
                 for (int k = i; k < j; k++) {
                     t += arr[k];
                 }
                 if (t > maxsum) {
                     maxsum = t;
+                    System.out.println(String.format("maxsum: %s, i: %s, j: %s", maxsum, i, j-1));
                 }
             }
+        }
+        return maxsum;
+    }
+
+    //without index
+    public int maxsumofSubarray2 (int[] arr) {
+        int maxsum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int t = 0;
+            for (int j = i; j < arr.length; j++) {
+                t += arr[j];
+                if (t > maxsum) {
+                    maxsum = t;
+                    System.out.println(String.format("maxsum: %s, i: %s, j: %s", maxsum, i, j));
+                }
+            }
+
         }
         return maxsum;
     }
