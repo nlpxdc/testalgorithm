f4fb09b35f1fe53ce35989119265852b90017079
nlpxdc <nlpx_dc@hotmail.com>
2021-05-16 11:16:53 +0800

    optimize bubble sort

--- src/main/java/io/cjf/testalgorithm/sort/bubblesort/Solution.java
+++ src/main/java/io/cjf/testalgorithm/sort/bubblesort/Solution.java
@@ -4,14 +4,21 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] numbers = new int[]{2, 3, 1};
+        int[] numbers2 = new int[]{1, 2, 3};
         Solution solution = new Solution();
+
         solution.bubbleSort(numbers);
+        solution.bubbleSort(numbers2);
     }
 
     public void bubbleSort(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
+        boolean beSort = false;
+        for (int i = 0; i < numbers.length && !beSort; i++) {
+            System.out.println(String.format("i: %s", i));
+            beSort = true;
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i] > numbers[j]) {
+                    beSort = false;
                     int t = numbers[i];
                     numbers[i] = numbers[j];
                     numbers[j] = t;
