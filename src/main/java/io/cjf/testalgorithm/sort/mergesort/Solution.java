ba8cb4c2f7349ee940a9f2f8980a10a47893106f
nlpxdc <nlpx_dc@hotmail.com>
2021-05-16 14:26:03 +0800

    fix

--- src/main/java/io/cjf/testalgorithm/sort/mergesort/Solution.java
+++ src/main/java/io/cjf/testalgorithm/sort/mergesort/Solution.java
@@ -3,19 +3,23 @@ package io.cjf.testalgorithm.sort.mergesort;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] left = {1, 3, 5, 7};
-        int[] right = {2, 4, 6, 8};
+//        int[] left = {1, 3, 5, 7};
+//        int[] right = {2, 4, 6, 8};
 
         int[] numbers = new int[]{2, 3, 1, 5,4,8,4,7};
+//        int[] numbers = new int[]{2, 3};
 
         Solution solution = new Solution();
-        int[] merge = solution.merge(left, right);
+//        int[] merge = solution.merge(left, right);
 
         int[] sort = solution.mergeSort(numbers);
 
     }
 
     public int[] mergeSort(int[] numbers) {
+        if (numbers.length < 2) {
+            return numbers;
+        }
         int m = numbers.length / 2;
         int[] left = new int[m];
         int[] right = new int[numbers.length - m];
@@ -41,21 +45,31 @@ public class Solution {
             if (left[i] < right[j]) {
                 result[c] = left[i];
                 i++;
-            } else {
+                c++;
+            } else if (left[i] > right[j]){
                 result[c] = right[j];
                 j++;
+                c++;
+            } else {
+                result[c] = left[i];
+                i++;
+                c++;
+                result[c] = right[j];
+                j++;
+                c++;
             }
-            c++;
         }
 
         while (i < left.length)  {
             result[c] = left[i];
             i++;
+            c++;
         }
 
         while (j < right.length) {
             result[c] = right[j];
             j++;
+            c++;
         }
 
         return result;
