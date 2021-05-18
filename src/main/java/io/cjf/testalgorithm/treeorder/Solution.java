6f3d2f48ffa8a5fcf51c81e4dc3d83524b3e6966
nlpxdc <nlpx_dc@hotmail.com>
2021-05-19 00:33:47 +0800

    draft

---
+++ src/main/java/io/cjf/testalgorithm/treeorder/Solution.java
@@ -0,0 +1,13 @@
+package io.cjf.testalgorithm.treeorder;
+
+public class Solution {
+    /**
+     *
+     * @param root TreeNode类 the root of binary tree
+     * @return int整型二维数组
+     */
+    public int[][] threeOrders (TreeNode root) {
+        // write code here
+        return null;
+    }
+}
