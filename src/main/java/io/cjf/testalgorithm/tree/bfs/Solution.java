d5d2293684966eb66810e4065f4c4222b3fe76fd
nlpxdc <nlpx_dc@hotmail.com>
2021-05-19 00:46:31 +0800

    draft

--- src/main/java/io/cjf/testalgorithm/tree/bfs/Solution.java
+++ src/main/java/io/cjf/testalgorithm/tree/bfs/Solution.java
@@ -3,6 +3,22 @@ package io.cjf.testalgorithm.tree.bfs;
 import java.util.ArrayList;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n20 = new TreeNode(20);
+        TreeNode n15 = new TreeNode(15);
+        TreeNode n7 = new TreeNode(7);
+
+        n3.left = n9;
+        n3.right = n20;
+
+        n20.left = n15;
+        n20.right = n7;
+
+    }
+
     /**
      *
      * @param root TreeNode类
