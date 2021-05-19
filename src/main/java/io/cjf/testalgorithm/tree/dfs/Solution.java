310afa9a8e580f19ba4542ba2cab1ca3918f2b9b
nlpxdc <nlpx_dc@hotmail.com>
2021-05-19 19:10:53 +0800

    add intraverse

--- src/main/java/io/cjf/testalgorithm/tree/dfs/Solution.java
+++ src/main/java/io/cjf/testalgorithm/tree/dfs/Solution.java
@@ -8,17 +8,18 @@ import java.util.Stack;
 public class Solution {
 
     public static void main(String[] args) {
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n9 = new TreeNode(9);
-        TreeNode n20 = new TreeNode(20);
-        TreeNode n15 = new TreeNode(15);
-        TreeNode n7 = new TreeNode(7);
+//        TreeNode n3 = new TreeNode(3);
+//        TreeNode n9 = new TreeNode(9);
+//        TreeNode n20 = new TreeNode(20);
+//        TreeNode n15 = new TreeNode(15);
+//        TreeNode n7 = new TreeNode(7);
+//
+//        n3.left = n9;
+//        n3.right = n20;
+//
+//        n20.left = n15;
+//        n20.right = n7;
 
-        n3.left = n9;
-        n3.right = n20;
-
-        n20.left = n15;
-        n20.right = n7;
 //        TreeNode n1 = new TreeNode(1);
 //        TreeNode n2 = new TreeNode(2);
 //        TreeNode n3 = new TreeNode(3);
@@ -26,9 +27,20 @@ public class Solution {
 //        n1.left = n2;
 //        n1.right = n3;
 
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+
+        n3.left = n2;
+        n3.right = n4;
+        n2.left = n1;
+        n4.right = n5;
+
         Solution solution = new Solution();
 //        List<Integer> integers = solution.preTraverse(n3);
-        List<Integer> integers = solution.postTraverse(n3);
+        List<Integer> integers = solution.inTraverse(n3);
 
 //        int[][] ints = solution.threeOrders(n3);
 
@@ -52,16 +64,25 @@ public class Solution {
         return ret;
     }
 
-//    private List<Integer> inTraverse(TreeNode root) {
-//        LinkedList<Integer> ret = new LinkedList<>();
-//        Stack<TreeNode> stack = new Stack<>();
-//        if (root != null) {
-//            for (int i = 0; i <; i++) {
-//
-//            }
-//        }
-//        return ret;
-//    }
+    private List<Integer> inTraverse(TreeNode root) {
+        LinkedList<Integer> ret = new LinkedList<>();
+        if (root != null) {
+            Stack<TreeNode> toVisits = new Stack<>();
+            TreeNode c = root;
+            while (!toVisits.isEmpty() || c != null) {
+                if (c != null) {
+                    toVisits.push(c);
+                    c = c.left;
+                } else {
+                    c = toVisits.pop();
+                    ret.add(c.val);
+                    c = c.right;
+                }
+            }
+
+        }
+        return ret;
+    }
 
     private List<Integer> postTraverse(TreeNode root) {
         LinkedList<Integer> ret = new LinkedList<>();
